f2605bb8f24c7603744b8aee424ce5bb274f6ec1
Gotsopoulos Athanasios <gostopa1@jouni.nbe.aalto.fi>
2017-11-03 01:14:35 +0200

    Find the first layer where lr~=0

---
+++ aux_functions/find_first_non_zero_layer.m
@@ -0,0 +1,11 @@
+function first_non_zero_layer_index=find_first_non_zero_layer(model)
+
+first_non_zero_layer_index=1;
+for layeri=length(model.layers):-1:1
+    if model.layers(layeri).lr==0
+        first_non_zero_layer_index=layeri;
+    end
+end
+
+
+end
