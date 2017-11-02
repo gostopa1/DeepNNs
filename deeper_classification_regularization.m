b0e9892f7710b2ed681fc912c6f2835e6e9f84c1
Athanasios Gotsopoulos <athanasios.gotsopoulos@aalto.fi>
2017-11-02 13:44:56 +0200

    Fixed issue with NaNs in regularization

--- deeper_classification_regularization.m
+++ deeper_classification_regularization.m
@@ -26,8 +26,8 @@ test_data=zscore([x_test(: ) y_test(:)],1);
 %% Model Initialization
 clear model
 
-layers=[5 ];
-model.batchsize=500
+layers=[10];
+model.batchsize=200
 noins=size(x,2);
 noouts=size(y,2);
 
@@ -42,7 +42,7 @@ model.target=y;
 model.epochs=500;
 model.update=100;
 model.l2=1;
-model.l1=1;
+model.l1=0;
 
 model.errofun='quadratic_cost';
 model.errofun='cross_entropy_cost';
@@ -58,16 +58,30 @@ for layeri=1:(length(layers)-1)
     model.layers(layeri).activation=activation;
 end
 
+%model.layers(layeri).lr=lr; model.layers(layeri).activation='softmaxact';
 model.layers(layeri).lr=lr; model.layers(layeri).activation='softmaxact';
 %% Model training
 
 clear error
 
 for epoch=1:model.epochs
+    model.epoch=epoch;
     if mod(epoch,model.update)==0
-        display(['Epoch: ' num2str(epoch)])
+        outemp=out(:,:,model.epoch-1);
+        %display(['Epoch: ' num2str(model.epoch)])
+        if sum(isfield(model,{'x_test','y_test'}))==2
+            epochtemp=model.epoch; model.epoch=0;
+            [~,out_test]=forwardpassing(model,[model.x_test]); get_perf(out_test,model.y_test);
+            model.epoch=epochtemp;
+            y_test_str=[' - Test: ' sprintf('%2.2f', get_perf(out_test,model.y_test))];
+        else
+            y_test_str='';
+        end
+        display(['Epoch: ' num2str(model.epoch) ' - Training: ' sprintf('%2.2f',get_perf(outemp,model.target)) y_test_str]);
+
     end
     %%Forward passing
+    prev_model=model;
     model=vectorize_all_weights(model);
     [model,out(:,:,epoch)]=forwardpassing(model,x);
     outnow=out(:,:,epoch);
@@ -75,10 +89,11 @@ for epoch=1:model.epochs
     [error(epoch),dedout]=feval(model.errofun,model);
     
     randomized_sample_indices=randperm(N); % Randomize the sample to randomly select samples for mini-batch
-    randomized_sample_indices=[randomized_sample_indices randomized_sample_indices];
+    randomized_sample_indices=[randomized_sample_indices randomized_sample_indices]; % In case N is not a integer multiple of the the minibatch size, this avoids errors by adding the same vector twice
     for batchi=1:model.batchsize:N
         for layeri=(length(model.layers)):-1:1
             clear dedw dedb
+            
             ins=model.layers(layeri).Ws(1);
             outs=model.layers(layeri).Ws(2);
             batchinds=randomized_sample_indices(batchi:(model.batchsize+batchi-1));
@@ -89,6 +104,7 @@ for epoch=1:model.epochs
                 
                 dedb=(dedout(batchinds,:).*model.layers(layeri).doutdnet(batchinds,:))'; % dE/db = dE/dout * dout/dnet
                 dedw=permute(repmat(dedb,1,1,ins),[3 1 2]).*dnetdw; % dE/dw = dE/dout * dout/dnet * dnet/d
+                
             else
                 dedb=(permute(sum(permute(repmat(model.layers(layeri+1).grad,1,1,outs),[3 2 1]).*repmat(model.layers(layeri+1).W,1,1,model.batchsize),2),[1 3 2])'.*model.layers(layeri).doutdnet(batchinds,:))';
                 dedw=permute(repmat(dedb,1,1,ins),[3 1 2]).*permute(repmat(model.layers(layeri).X(batchinds,:),1,1,outs),[2 3 1]);
@@ -96,9 +112,12 @@ for epoch=1:model.epochs
             
             model.layers(layeri).grad=dedb';
             if model.layers(layeri).lr~=0
-                %regularization_term1=model.layers(layeri).W-(model.layers(layeri).lr*((model.l2*sum(model.allweights(:)))/model.batchsize)).*model.layers(layeri).W;
-                %regularization_term2=model.layers(layeri).W-(model.layers(layeri).lr*((model.l2*sum(model.allweights(:)))/model.batchsize)).*sign(model.layers(layeri).W);
-                regularization_term=model.layers(layeri).W-(model.layers(layeri).lr*((model.l1*sum(model.allweights(:)))/model.batchsize)).*sign(model.layers(layeri).W)-(model.layers(layeri).lr*((model.l2*sum(model.allweights(:)))/model.batchsize)).*model.layers(layeri).W;
+                
+                
+                l1part=-model.layers(layeri).lr*(model.l1/model.batchsize).*sign(model.layers(layeri).W);
+                l2part=-model.layers(layeri).lr*(model.l2/model.batchsize).*model.layers(layeri).W;
+                regularization_term=model.layers(layeri).W+l1part+l2part;
+                
                 temp=regularization_term-model.layers(layeri).lr*mean(dedw,3);
                 if sum(isnan(temp))>0
                     display('About to explode')
