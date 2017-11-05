0515fba90721c578dbb22885a6a18377e0c7518e
Athanasios Gotsopoulos <athanasios.gotsopoulos@aalto.fi>
2017-11-05 19:15:06 +0200

    Added forward nolr function

---
+++ forwardpassing_nolr.m
@@ -0,0 +1,22 @@
+function [model,out]=forwardpassing_nolr(model,x)
+model.layers(1).X=x;
+layerno=length(model.layers);
+for layeri=model.fnzl:layerno
+    if sum(model.layers(layeri).lr(:))~=0
+        if strcmp(model.layers(layeri).activation,'softmaxact')
+            [model.layers(layeri).out, model.layers(layeri).doutdnet]=feval(model.layers(layeri).activation,model.layers(layeri).X,model.layers(layeri).W,model.layers(layeri).B,model.target);
+        else
+            [model.layers(layeri).out, model.layers(layeri).doutdnet]=feval(model.layers(layeri).activation,model.layers(layeri).X,model.layers(layeri).W,model.layers(layeri).B);
+        end
+  
+    %% If we are in the last layer, set the output of the model as the layer's output. Else set its output as the input to the next one.
+    if layeri==layerno
+        out=model.layers(layeri).out;
+    else
+        model.layers(layeri+1).X=model.layers(layeri).out;
+        
+    end
+    end
+end
+
+end
