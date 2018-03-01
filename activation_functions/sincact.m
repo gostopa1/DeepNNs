7f77111ed34d5dcf5dce18ad2ec680c934284b56
Athanasios Gotsopoulos <tan@neuraldsp.com>
2018-03-01 11:45:51 +0200

    Fixed the numerical derivatives of sinc (minor fixes, noone uses sinc anyways :) )

--- activation_functions/sincact.m
+++ activation_functions/sincact.m
@@ -1,9 +1,9 @@
 function [act,der]=softsign(X,W,B)
 preact=X*W+repmat(B,1,size(X,1))';
 act=zeros(size(preact));
-act(preact~=0)=sin(preact)./preact;
+act(preact~=0)=sin(preact(preact~=0))./preact(preact~=0);
 
 der=zeros(size(preact));
-der(preact~=0)=(cos(preact)./preact)-(sin(preact)./(preact.^2));
+der(preact~=0)=(cos(preact(preact~=0))./preact(preact~=0))-(sin(preact(preact~=0))./(preact(preact~=0).^2));
 
 end
