02d3f3596f48a5aec58e04dda55aeec00ce0fa01
Athanasios Gotsopoulos <athanasios.gotsopoulos@aalto.fi>
2018-06-30 21:18:06 +0300

    Updated the importance extraction functions

--- importance_extraction/extract_LRP_lowmem.m
+++ importance_extraction/extract_LRP_lowmem.m
@@ -1,4 +1,4 @@
-function [impos,imps_per_layer]=extract_LRP(model,x)
+function [impos,imps_per_layer]=extract_LRP_lowmem(model,x)
 % This estimates the importances (or relevance) of the inputs, as well as
 % the importances for each node (second arguments) based on the article by
 % Montavon et al., 2017
@@ -19,15 +19,14 @@ nosamples=size(x,1);
 noouts=model.layersizes(end);
 for outi=1:noouts
     R=model.layers(length(model.layers)).out;
-    %R=model.layers(length(model.layers)).out*0+1;    
+    %R=model.layers(length(model.layers)).out*0+1;
     R(:,setxor(1:noouts,outi))=0;
-    %R=mean(R,1); nosamples=1;
+    R=mean(R,1); nosamples=1; % Add this to take only the average output of all the samples
     for layeri=length(model.layers):-1:1
         n=model.layersizes(layeri+1);
         m=model.layersizes(layeri);
         
         %Now it implements simple W.^2 importance extraction
-        
         %Wr=permute(repmat(model.layers(layeri).W,1,1,nosamples),[3 1 2]);
         %Xr=repmat(model.layers(layeri).X,1,1,n);
         %Br=permute(repmat(model.layers(layeri).B,1,model.layersizes(layeri),nosamples),[3 2 1]);
@@ -36,10 +35,10 @@ for outi=1:noouts
         Z=permute(repmat(full(model.layers(layeri).W.^2),1,1,nosamples),[3 1 2]);
         Zs=sum(Z,2);
         Rr=repmat(permute(R,[1 3 2]),1,m,1);
-        R=sum((Z./repmat(Zs,1,m,1)).*Rr,3);
-
+        R=sum((Z./repmat(Zs,1,m,1)).*Rr,3);    
         imps_per_layer(layeri).R=R;
     end
+    
     impos(:,:,outi)=R;
     
 end
