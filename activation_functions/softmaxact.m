59e294feb23563244581ff79619ddde7821c49e3
Athanasios Gotsopoulos <tan@neuraldsp.com>
2018-08-07 12:53:39 +0300

    Fixed softmax derivative and its dependance to target

--- activation_functions/softmaxact.m
+++ activation_functions/softmaxact.m
@@ -1,10 +1,10 @@
-function [act,der]=softmaxact(X,W,B,target)
+function [act,der]=softmaxact(X,W,B)
 
 preact=X*W+repmat(B,1,size(X,1))';
 
 exppreact=exp(preact);
 
-act=exppreact./repmat(sum(exppreact,2),1,size(exppreact,2));
+act=exppreact./repmat(sum(exppreact,2),1, size(exppreact,2));
 
 upthres=0.99; downthres=0.01;
 
@@ -13,8 +13,8 @@ act(act<downthres)=downthres;
 
 try
     
-    %der=(act.*(target-act));
-    der=ones(size(act)); % Assigning the derivatives to ones, seems to work better without a sound explanation. Otherwise, when using softmax and cross-entropy, the product of their derivatives (i.e. dE/net=dE/dout * dout/dnet) is always positive, that makes the weights in the output layer to grow infinitely and explode.
+    der=act.*(1-act); % This corresponds to the diagonal of the Jacobian
+    %der=ones(size(act)); % Assigning the derivatives to ones, seems to work better without a sound explanation. Otherwise, when using softmax and cross-entropy, the product of their derivatives (i.e. dE/net=dE/dout * dout/dnet) is always positive, that makes the weights in the output layer to grow infinitely and explode.
 catch
     display('Number of samples do not match between inputs and targets! Cannot estimate derivative. Setting them to zero instead!')
     der=zeros(size(act));
