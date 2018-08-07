b84585e32ee70399faaab5b1f82bdb6bdfb1030e
Athanasios Gotsopoulos <tan@neuraldsp.com>
2018-08-07 14:24:14 +0300

    Reverted gradient to ones, proper gradient is way slower (??)

--- activation_functions/softmaxact.m
+++ activation_functions/softmaxact.m
@@ -6,20 +6,12 @@ exppreact=exp(preact);
 
 act=exppreact./repmat(sum(exppreact,2),1, size(exppreact,2));
 
-upthres=0.99; downthres=0.01;
+% upthres=0.99; downthres=0.01;
+% 
+% act(act>upthres)=upthres;
+% act(act<downthres)=downthres;
 
-act(act>upthres)=upthres;
-act(act<downthres)=downthres;
-
-try
-    
-    der=act.*(1-act); % This corresponds to the diagonal of the Jacobian
-    %der=ones(size(act)); % Assigning the derivatives to ones, seems to work better without a sound explanation. Otherwise, when using softmax and cross-entropy, the product of their derivatives (i.e. dE/net=dE/dout * dout/dnet) is always positive, that makes the weights in the output layer to grow infinitely and explode.
-catch
-    display('Number of samples do not match between inputs and targets! Cannot estimate derivative. Setting them to zero instead!')
-    der=zeros(size(act));
-end
-
-
+%der=act.*(1-act); % This corresponds to the diagonal of the Jacobian
+der=ones(size(act)); % Assigning the derivatives to ones, seems to work better without a sound explanation. Otherwise, when using softmax and cross-entropy, the product of their derivatives (i.e. dE/net=dE/dout * dout/dnet) is always positive, that makes the weights in the output layer to grow infinitely and explode.
 
 end
