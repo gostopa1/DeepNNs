556eb935164f1c33177ea11ddd8c4b4af9e8c269
Athanasios Gotsopoulos <athanasios.gotsopoulos@aalto.fi>
2023-10-17 22:26:26 +0300

    Added eps to address low values that cause NaNs to log and division

--- loss_functions/cross_entropy_cost.m
+++ loss_functions/cross_entropy_cost.m
@@ -1,22 +1,15 @@
 function [error,dedout]=cross_entropy_cost(model)
+%implementation based on this: https://sgugger.github.io/a-simple-neural-net-in-numpy.html
 
-% Restict values to range (0.001,0.999) to avoid NaNs and Infs in the
-% gradient calculation.
-
-%error=-sum(mean(model.target.*log(model.layers(end).out)+(1-model.target).*log(1-model.layers(end).out),1));
-error=-sum(sum(model.target.*log(model.layers(end).out)+(1-model.target).*log(1-model.layers(end).out),1))/model.batchsize;
-%error=-sum(sum(model.target.*log(model.layers(end).out)));
-error=error +model.l2/(2*model.batchsize)*sum(model.allweights(:).^2)+model.l1/(2*model.batchsize)*sum(abs(model.allweights(:)));
-%error=error -model.l2/(2*model.batchsize)*sum(model.allweights(:).^2)-model.l1/(2*model.batchsize)*sum(abs(model.allweights(:)));
-
-%dedout=-((model.target./model.layers(end).out)-((1-model.target)./(1-model.layers(end).out)));
-dedout=(model.layers(end).out)-(model.target);
+eps=1e-9;
+temp_out = model.layers(end).out;
+temp_out(abs(temp_out)<eps)=eps; % Remove too small values to address numerical issues in log and division
 
 
+error = -sum(sum(model.target.*log(temp_out)))/model.batchsize;
 
-uplim=0.99;
-lowlim=0.11;
-model.layers(end).out(model.layers(end).out>uplim)=uplim; 
-model.layers(end).out(model.layers(end).out<lowlim)=lowlim;
-error=real(error);
+error = error + model.l2/(2*model.batchsize)*sum(model.allweights(:).^2)+model.l1/(2*model.batchsize)*sum(abs(model.allweights(:)));
+
+dedout=model.target.*(-1./temp_out);
+
 end
