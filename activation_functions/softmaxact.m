1d3506da38d22a21f0c8ab9611f377c2e25c8051
Athanasios Gotsopoulos <athanasios.gotsopoulos@aalto.fi>
2023-10-22 02:03:25 +0300

    Improved numerical stability of softmax by removing the largest value per batch element

--- activation_functions/softmaxact.m
+++ activation_functions/softmaxact.m
@@ -2,10 +2,12 @@ function [act,der]=softmaxact(X,W,B)
 
 preact=X*W+repmat(B,1,size(X,1))';
 
+% Prevent overflows by removing the largest activation value per batch element
+preact=preact-max(preact,[],2); 
 exppreact=exp(preact);
 
 %Normalize to sum to 1
-act=exppreact./repmat(sum(exppreact,2),1, size(exppreact,2));
+act=exppreact./(repmat(sum(exppreact,2),1, size(exppreact,2)));
 
 der=act.*(1-act); % This corresponds to the diagonal of the Jacobian
 
