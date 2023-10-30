a0c8c57660f643ce84770bac1da07af513eea79b
Athanasios Gotsopoulos <athanasios.gotsopoulos@aalto.fi>
2023-10-30 02:30:21 +0200

    Removed commented out code and minor updates

--- loss_functions/calc_cross_entropy.m
+++ loss_functions/calc_cross_entropy.m
@@ -3,13 +3,9 @@ function [error]=calc_cross_entropy(y_target, y_predicted)
 % Restict values to range (0.001,0.999) to avoid NaNs and Infs in the
 % gradient calculation.
 batchsize=size(y_target,1);
-error = -sum(sum(y_target.*log(y_predicted)+(1-y_target).*log(1-y_predicted),1))/batchsize;
 
-%error = error + model.l2/(2*model.batchsize)*sum(model.allweights(:).^2)+model.l1/(2*model.batchsize)*sum(abs(model.allweights(:)));
-%error = error -model.l2/(2*model.batchsize)*sum(model.allweights(:).^2)-model.l1/(2*model.batchsize)*sum(abs(model.allweights(:)));
+eps=1e-9;
+y_predicted(y_predicted(:)<eps)=eps;
+error = -sum(sum(y_target.*log(y_predicted)))/batchsize;
 
-uplim=0.99;
-lowlim=0.11;
-
-error=real(error);
 end
--- loss_functions/cross_entropy_cost.m
+++ loss_functions/cross_entropy_cost.m
@@ -3,13 +3,13 @@ function [error,dedout]=cross_entropy_cost(model)
 
 eps=1e-9;
 temp_out = model.layers(end).out;
-temp_out(abs(temp_out)<eps)=eps; % Remove too small values to address numerical issues in log and division
+temp_out(temp_out(:)<eps)=eps; % Remove too small values to address numerical issues in log and division
 
+temp_error = model.target.*log(temp_out);
+%sum(temp_error)
 error = -sum(sum(model.target.*log(temp_out)))/model.batchsize;
 
 error = error + model.l2/(2*model.batchsize)*sum(model.allweights(:).^2)+model.l1/(2*model.batchsize)*sum(abs(model.allweights(:)));
 
-dedout=-model.target.*(1./temp_out);
-dedout = tanh(dedout); % To restrict values to avoid exploding gradients
-
+dedout=model.target.*(-1./temp_out);
 end
